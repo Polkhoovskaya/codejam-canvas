110d07dfe6368be2887ee6ad8b66ff691c74d11d
VolhaP <polkhovskayaolga@mail.ru>
2019-11-30 19:45:20 +0300

    fix: add addCanvas, delete addCanvas1Data and addCanvas2Data

--- script.js
+++ script.js
@@ -23,45 +23,33 @@ fetch(CANVAS_IMG2).then(response => {
   console.log('The request failed!'); 
 });
 
-let addCanvas1Data = () => {
- 
 
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    let width = canvas1[0].length;
-    let height = canvas1.length;
-    let scale = 512/width;
+let addCanvas = (canvasData) => {
 
-    canvas.width = width * scale; 
-    canvas.height = height * scale;
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+  let width = canvasData[0].length;
+  let height = canvasData.length;
+  let scale = 512/width;
 
-    // Loop through each color and draw that section
-    for(let row = 0; row < height; row++) {
-        for(let col = 0; col < width; col++) { 
-            ctx.fillStyle = '#' + canvas1[row][col]; 
+  canvas.width = width * scale; 
+  canvas.height = height * scale;
+
+  // Loop through each color and draw that section
+  for(let row = 0; row < height; row++) {
+      for(let col = 0; col < width; col++) { 
+        if (canvasData[row][col].length == 4) {
+          ctx.fillStyle = 'rgba(' + canvasData[row][col][0] + ',' + canvasData[row][col][1]+ ',' + canvasData[row][col][2] + ',' +  canvasData[row][col][3] + ')'; 
             ctx.fillRect(col * scale, row * scale, scale, scale); 
         }
-    }
-  }
-let addCanvas2Data = () => {
-
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    let width = canvas2[0].length;
-    let height = canvas2.length;
-    let scale = 512/width;
-
-    canvas.width = width * scale; 
-    canvas.height = height * scale;
-
-    // Loop through each color and draw that section
-    for(let row = 0; row < height; row++) {
-        for(let col = 0; col < width; col++) { 
-            ctx.fillStyle = 'rgba(' + canvas2[row][col][0] + ',' + canvas2[row][col][1]+ ',' + canvas2[row][col][2] + ',' +  canvas2[row][col][3] + ')'; 
-            ctx.fillRect(col * scale, row * scale, scale, scale); 
-        }
-    }
+        else {
+          ctx.fillStyle = '#' + canvasData[row][col]; 
+          ctx.fillRect(col * scale, row * scale, scale, scale);
+        } 
+      }
+    } 
 }
+
 let addImg = () => {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
@@ -73,10 +61,10 @@ let addImg = () => {
 }
 
 let button1 = document.getElementById('button1');
-button1.onclick = addCanvas1Data;
+button1.onclick = () => addCanvas(canvas1);
 
 let button2 = document.getElementById('button2');
-button2.onclick = addCanvas2Data;
+button2.onclick = () => addCanvas(canvas2);
 
 let button3 = document.getElementById('button3');
 button3.onclick = addImg;
